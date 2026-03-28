8620fe1b5915d5f23569c8d99a54faae2eec7a3a
Rinaldo Lee <37181282+iwillreku3206@users.noreply.github.com>
2026-03-28 19:39:49 +0800

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from '@astrojs/mdx';
-import wasm from 'vite-plugin-wasm'
-import wasmPack from 'vite-plugin-wasm-pack';
-import topLevelAwait from 'vite-plugin-top-level-await'
+import mdx from "@astrojs/mdx";
+import wasm from "vite-plugin-wasm";
+import wasmPack from "vite-plugin-wasm-pack";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 import svelte from "@astrojs/svelte";
 import { remarkReadingTime } from "./remark/remark-reading-time.mjs";
@@ -13,12 +13,25 @@ export default defineConfig({
   srcDir: "src",
   integrations: [tailwind(), mdx(), svelte(), wasm()],
   vite: {
-    plugins: [wasm(), topLevelAwait(), wasmPack('rinaldolee_com_wasm')],
+    plugins: [wasm(), topLevelAwait(), wasmPack("rinaldolee_com_wasm")],
     resolve: {
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.svelte', '.md', '.mdx', '.wasm']
-    }
+      extensions: [
+        ".mjs",
+        ".js",
+        ".ts",
+        ".jsx",
+        ".tsx",
+        ".json",
+        ".vue",
+        ".svelte",
+        ".md",
+        ".mdx",
+        ".wasm",
+      ],
+    },
   },
   markdown: {
-    remarkPlugins: [remarkReadingTime]
-  }
+    remarkPlugins: [remarkReadingTime],
+  },
+  site: "https://rinaldolee.com",
 });
